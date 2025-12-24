a0cb497d708cc2227d0a2867ae728fccefbb1fdc
king-nourou <nourou003@hotmail.com>
2025-12-24 13:30:53 +0200

    Update

--- CISSE_DAMI_projet_bs/sdl.cpp
+++ CISSE_DAMI_projet_bs/sdl.cpp
@@ -1,5 +1,88 @@
 /**
  * @file sdl.cpp
- * @brief implémentation de la classe sdl
+ * @brief Implémentation de la classe Sdl
  */
+
 #include "sdl.hpp"
+#include <iostream>
+
+/**
+ * @brief Constructeur de la classe Sdl
+ * @param height longeur de la fenêtre
+ * @param width largeur de la fenêtre
+ * @param titre titre de la fenêtre
+ */
+Sdl::Sdl(int height, int width, std::string titre)
+	: height_(height), width_(width), titre_(titre), running_(true)
+{
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		std::cerr << "Erreur Init SDL" << std::endl;
+		running_ = false;
+	}
+
+	// Création de la fenêtre
+	window_ = SDL_CreateWindow(
+		titre_.c_str(),
+		SDL_WINDOWPOS_CENTERED,
+		SDL_WINDOWPOS_CENTERED,
+		width_,
+		height_,
+		SDL_WINDOW_SHOWN);
+
+	// Crétion du renderer
+	renderer_ = SDL_CreateRenderer(window_, -1, SDL_RENDERER_ACCELERATED);
+}
+
+/**
+ * @brief Destructeur de la classe Sdl
+ */
+Sdl::~Sdl()
+{
+	// Détruire renderer_
+	SDL_DestroyRenderer(renderer_);
+
+	// Détruire window
+	SDL_DestroyWindow(window_);
+
+	// Quitter SDL
+	SDL_Quit();
+}
+
+/**
+ * @brief gère le flag pour l'ouverture de la fenêtre
+ */
+bool Sdl::isRunning()
+{
+	SDL_Event e;
+	while (SDL_PollEvent(&e))
+	{
+		if (e.type == SDL_QUIT)
+		{
+			running_ = false;
+		}
+	}
+	return running_;
+}
+
+/**
+ * @brief nettoyage de la fenêtre
+ */
+void Sdl::clear()
+{
+	SDL_SetRenderDrawColor(renderer_, 255, 255, 255, 255);
+	SDL_RenderClear(renderer_);
+}
+
+/**
+ * @brief dessine la courbe sur la fenêtre
+ * @param x prix de l'actif sous-jacent
+ * @param y valeurs de l'option
+ * @param x_max prix maximum de l'actif
+ * @param y_max valeur maximum de l'option
+ * @param color couleur de la courbe
+ */
+void drawCurve(const std::vector<double> &x, const std::vector<double> &y, double x_max, double y_max, SDL_Color color)
+{
+	return;
+}
--- CISSE_DAMI_projet_bs/sdl.hpp
+++ CISSE_DAMI_projet_bs/sdl.hpp
@@ -1,6 +1,6 @@
 /**
  * @file sdl.hpp
- * @brief class sdl pour l'affichage des courbes de la prix des options en fonction du prix de l'actif sous-jacent
+ * @brief Déclaration de la class sdl pour l'affichage des courbes de la prix des options en fonction du prix de l'actif sous-jacent
  */
 
 #ifndef SDL_HPP
@@ -10,7 +10,60 @@
 #include <vector>
 #include <string>
 
+/**
+ * @class sdl
+ * @brief class sdl pour l'affichage des courbes de la prix des options en fonction du prix de l'actif sous-jacent
+ */
 class Sdl
 {
+private:
+	SDL_Window *window_;
+	SDL_Render *renderer_;
+	int height_;
+	int width_;
+	bool running_;
+	std::string titre_;
+
+public:
+	/**
+	 * @brief Constructeur de la classe Sdl
+	 * @param height longeur de la fenêtre
+	 * @param width largeur de la fenêtre
+	 * @param titre titre de la fenêtre
+	 */
+	Sdl(int height, int width, std::string titre_);
+
+	/**
+	 * @brief Destructeur de la classe Sdl
+	 */
+	~Sdl();
+
+	/**
+	 * @brief
+	 */
+	SDL_Renderer *present()
+	{
+		SDL_RenderPresent(renderer_);
+	}
+
+	/**
+	 * @brief gère le flag pour l'ouverture de la fenêtre
+	 */
+	bool isRunning();
+
+	/**
+	 * @brief dessine la courbe sur la fenêtre
+	 * @param x prix de l'actif sous-jacent
+	 * @param y valeurs de l'option
+	 * @param x_max prix maximum de l'actif
+	 * @param y_max valeur maximum de l'option
+	 * @param color couleur de la courbe
+	 */
+	void drawCurve(const std::vector<double> &x, const std::vector<double> &y, double x_max, double y_max, SDL_Color color);
+
+	/**
+	 * @brief nettoyage de la fenêtre
+	 */
+	void clear();
 }
 #endif
